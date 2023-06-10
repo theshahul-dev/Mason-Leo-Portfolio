6f66826744eb32b10b6fc506bea8dbb7c90aae18
theshahul-dev <theshahuldev@gmail.com>
2023-06-10 20:31:57 +0530

    scrll reveal added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,5 @@
 // ScrollReveal().reveal('.scrollreveal2',{distance:'100px',delay:600,origin:'left',reset:true});
 // ScrollReveal().reveal('.scrollreveal3',{distance:'100px',delay:700,origin:'left',reset:true});
 // ScrollReveal().reveal('.scrollreveal4',{distance:'100px',delay:800,origin:'left',reset:true});
-// ScrollReveal().reveal('.scrollrevealleft',{distance:'100px',delay:500,origin:'left',reset:true});
-// ScrollReveal().reveal('.scrollrevealright',{distance:'100px',delay:500,origin:'right',reset:true});
+ScrollReveal().reveal('.scrollrevealleft',{distance:'100px',delay:500,origin:'left',reset:true});
+ScrollReveal().reveal('.scrollrevealright',{distance:'100px',delay:500,origin:'right',reset:true});
